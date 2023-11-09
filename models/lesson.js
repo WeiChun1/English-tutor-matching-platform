124bb3e2eeac917feadd575eeadd1f80d546297a
andybox111 <andybox11111@gmail>
2023-11-09 14:22:54 +0800

    modify lessons model

--- models/lesson.js
+++ models/lesson.js
@@ -27,7 +27,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Lesson',
-    modelTable: 'Lessons',
+    tableName: 'Lessons',
     underscored: true,
   });
   return Lesson;
